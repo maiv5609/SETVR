8c73298ae717a6c422ac0e34744f4d552c35bdaa
maiv5609 <victormai5609@outlook.com>
2018-05-29 21:16:12 -0700

    Added question importing, logic to move from phase 1 through 3 and random picking from phase 2

---
+++ Assets/Scripts/createQuestionRef.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class createQuestionRef : MonoBehaviour {
+
+	Text questionText;
+
+	// Initialization for current question ref
+	void Start () {
+		questionText = GetComponent<Text>();
+	}
+	
+}
