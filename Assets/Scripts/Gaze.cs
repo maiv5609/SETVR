c076aaef0483ca58d73ed373dc9d3823468ec0ae
GeorgeDubuque <gtdubuque@gmail.com>
2018-05-30 22:24:16 -0700

    added free 3d model of man in suit and ability to detect if player is looking at interviewers head, also removed all unneccesary environment objects and re-orginized the game object heirarchy

---
+++ Assets/Scripts/Gaze.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour {
+
+	public float attentionTime;
+	private bool prevView = false;
+	private float currTime;
+	private float prevTime;
+
+	void Awake(){
+
+		currTime = Time.time;
+		prevTime = Time.time;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		currTime = Time.time;
+
+		int layerMask = 1 << 8;
+
+		RaycastHit hit;
+		bool payingAttention = Physics.Raycast (transform.position, transform.forward, Mathf.Infinity, layerMask);
+		Debug.DrawRay (transform.position, transform.forward * 100f, Color.red);
+
+		if (payingAttention) {
+
+			prevTime = Time.time;
+		}
+
+		if (!prevView && payingAttention) {
+
+			print (payingAttention);
+			prevView = true;
+		}
+
+		if (prevView && !payingAttention) {
+
+			print (payingAttention);
+			prevView = false;
+		}
+
+		//has user been looking away for longer than attentionTime seconds
+		if (currTime - prevTime >= attentionTime) {
+
+			//trigger interviewer to acknowledge that the user is not paying attention
+			print("User looked away for too long.");
+		}
+
+	}
+}
