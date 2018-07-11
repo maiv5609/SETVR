4e8436bbda9661b239b9269338a70a570dbd0742
maiv <maiv@wwu.edu>
2018-07-11 11:03:35 -0700

    Cleaned up gaze detection and added threshold

--- Assets/Scripts/Gaze.cs
+++ Assets/Scripts/Gaze.cs
@@ -6,7 +6,7 @@ using UnityEngine.Analytics;
 
 public class Gaze : MonoBehaviour {
 
-	public float lookAwayTime;
+	public float lookAwayTime; //Max time user can look away before being notified
 	private bool prevView = false;
 	private float currTime;
 	private float prevTime;
@@ -14,27 +14,21 @@ public class Gaze : MonoBehaviour {
 	private float distractionTime;
 	private float startAttention;
 	private float startDistracted;
+	float currCountdownValue;
 
 	//Variables for fading UI based on eye contact
 	public Image Icon; //Attach an image you want to fade in the GameObject's Inspector
 	bool fading; //Use this to tell if the toggle returns true or false
 
 	void Awake(){
-
+		lookAwayTime = 4; 
 		currTime = Time.time;
 		prevTime = Time.time;
 	}
 
 	void OnDestroy(){
-
 		print ("Attention Time: " + attentionTime);
 		print ("Distracted Time: " + distractionTime);
-
-		//Send information to Unity Analytics
-		AnalyticsEvent.Custom ("Focus_Time", new Dictionary<string, object> {
-			{"AttentionTime", attentionTime},
-			{"DistractedTime", distractionTime}
-		});
 	}
 
 	// Update is called once per frame
@@ -48,44 +42,51 @@ public class Gaze : MonoBehaviour {
 		bool payingAttention = Physics.Raycast (transform.position, transform.forward, Mathf.Infinity, layerMask);
 		Debug.DrawRay (transform.position, transform.forward * 100f, Color.red);
 
+		//Making Eye Contact
 		if (payingAttention) {
-
-			prevTime = Time.time;
+			changeFade (false);
 		}
 
+		//Making Eye Contact, tally
 		if (!prevView && payingAttention) {
-
 			attentionTime += currTime - startDistracted;
 			startAttention = Time.time;
 			prevView = true;
-			fading = false;
 		}
 
+		//Breaking Eye Contact
 		if (prevView && !payingAttention) {
-
 			distractionTime += currTime - startAttention;
 			startDistracted = Time.time;
 			prevView = false;
-			fading = true;
+			StartCoroutine (StartCountdown ());
 		}
+	}
 
+	//Countdown to track time spent not making eye contact
+	public IEnumerator StartCountdown(float countdownValue = 2)
+	{
+		currCountdownValue = countdownValue;
+		while (currCountdownValue > 0)
+		{
+			Debug.Log("Countdown: " + currCountdownValue);
+			yield return new WaitForSeconds(1.0f);
+			currCountdownValue--;
+		}
+		if (currCountdownValue == 0) {
+			changeFade (true);
+		}
+	}
+
+	//Adjusts fade options
+	public void changeFade(bool fade){
 		//If fading is true, fade in Gaze Indicator
-		if (fading == true) {
+		if (fade == true) {
 			Icon.CrossFadeAlpha(1, 2.0f, false);
 		}
 		//If fading is false, fade out Gaze Indicator
-		else if (fading == false) {
+		else if (fade == false) {
 			Icon.CrossFadeAlpha(0, 0.5f, false);
 		}
-
-		//has user been looking away for longer than attentionTime seconds
-		if (currTime - prevTime >= lookAwayTime) {
-
-			//trigger interviewer to acknowledge that the user is not paying attention
-			print("User looked away for too long.");
-		}
-
 	}
-
-
 }
