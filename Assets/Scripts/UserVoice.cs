dc2f24cf3709f51067b28e24354c2874e35ba32b
maiv <maiv@wwu.edu>
2018-07-17 09:56:40 -0700

    Fixed voice detection, will be testing voice recording and finishing question logic

--- Assets/Scripts/UserVoice.cs
+++ Assets/Scripts/UserVoice.cs
@@ -6,42 +6,60 @@ public class UserVoice : MonoBehaviour {
 
 	private Microphone mic;
 	public int voiceSampleSize;
-	public float isSpeakingThreshold;
+	public float sensitivity;
+	AudioClip microphoneInput;
+
+	//TODO: Recording working, Need to figure out restricting to clips
 
 	// Use this for initialization
 	void Awake () {
 
-		string[] devices = Microphone.devices;
 		AudioSource audio = GetComponent<AudioSource> ();
-		AudioClip sampleClip; //Test clip
+		sensitivity = 0.3f; // Sensitivity for mic input
 
-		if (devices.Length == 0) {
-
+		if (Microphone.devices.Length == 0) {
 			//pause and ask user to plug in mic
 			print("no microphone plugged in");
 		} else {
-			audio.clip = Microphone.Start(devices [0], true, 1200, 16000);
+			microphoneInput = Microphone.Start(Microphone.devices[0], true, 999, 44100);
 
-			//Testing recording audio clips
-			sampleClip = Microphone.Start(devices [0], true, 10, 44100);
-			SavWav.Save ("userVoiceTestFile", sampleClip);
 
-			audio.loop = true;
-			while (!(Microphone.GetPosition (null) > 0)) {}
-			audio.Play ();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		float currVoiceVolume = voiceVolume();
-		print(currVoiceVolume);
-		if (currVoiceVolume >= isSpeakingThreshold) {
+		//Get mic volume
+		int dec = 128;
+		float[] waveData = new float[dec];
+		int micPosition = Microphone.GetPosition(null) - (dec + 1); // null means the first microphone
+		microphoneInput.GetData(waveData, micPosition);
 
-			//do something cause the user spoke
-			print("user is speaking");
+		//Getting a peak from the last 128 samples
+		float levelMax = 0;
+		float wavePeak;
+		for (int i = 0; i < dec; i++) {
+			wavePeak = waveData [i] * waveData [i];
+			if (levelMax < wavePeak) {
+				levelMax = wavePeak;
+			}
 		}
+
+		float level = Mathf.Sqrt (Mathf.Sqrt (levelMax));
+
+		//Mic input is louder than set sensitivity 
+		if (level > sensitivity) {
+			Debug.Log ("Mic input detected");
+			Debug.Log ("Level: " + level);
+		}
+
+			
+	}
+
+	void OnApplicationQuit(){
+		//Save Audio to file
+		SavWav.Save ("microphoneInputTest", microphoneInput);
 	}
 
 	//takes volume data from microphone, averages it
@@ -63,4 +81,5 @@ public class UserVoice : MonoBehaviour {
 
 		return voiceVolume;
 	}
+		
 }
