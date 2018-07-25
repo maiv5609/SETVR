f0f804931beec5f5e9dec99923485621f1a5befe
maiv5609 <victormai5609@outlook.com>
2018-07-24 20:33:02 -0700

    Fixed issue with getting microphone data, need to continue work on detecting prolonged silence and nextquestion gate.

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//TODO: Error with GetData had to do with me closing the microphone when making clips
+// Currently working on detecting prolonged silence. There is an issue with questions continuely moving on 
+// when the mic is on.
+// Need to go into NextQUestion and add a gate (until the user has answered)
+
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -98,43 +103,43 @@ public class Questions : MonoBehaviour {
 				//Check if interviewer is talking (find the temp audiosource gameobject)
 				if (GameObject.Find ("One shot audio") && !interrupted) {
 					//User interrupted interviewer
+					Debug.Log("interrupt");
 					interrupted = true;
-				} else if (!userAnswered && !clipRecorded) {
+				} else if (!GameObject.Find ("One shot audio") && !userAnswered && !clipRecorded) {
 					//If user hasn't answered, audio clip hasn't been recorded yet
 					//This should only be used once per question to begin the recording
+					Debug.Log("Recording clip");
 					recordAudioClip();
 				} 
 
-				Debug.Log ("Level: " + level);
+				//Debug.Log ("Level: " + level);
 			} else {
 				//User is not speaking
-				if (clipRecorded && userAnswered && !GameObject.Find ("One shot audio")) {
-					Debug.Log ("User not");
+				if (userAnswered && !GameObject.Find ("One shot audio")) {
 					//Audio has been recorded and User has answered the question 
-
-					/*
-					micPosition = Microphone.GetPosition (null) - (1000 + 1); // null means the first microphone
+					//Check for silence
+					waveData = new float[500];
+					micPosition = Microphone.GetPosition (null) - (500 + 1); // null means the first microphone
 					microphoneInput.GetData (waveData, micPosition);
 					//Check if user has not said anything during the last couple seconds before moving on
 					levelMax = 0;
 					wavePeak = 0;
-					for (int i = 0; i < 1000; i++) {
+					for (int i = 0; i < 500; i++) {
 						wavePeak = waveData [i] * waveData [i];
 						if (levelMax < wavePeak) {
 							levelMax = wavePeak;
 						}
 					}
-					*/
-					
+					level = Mathf.Sqrt (Mathf.Sqrt (levelMax));
+
 					if (level < sensitivity) {
 						Debug.Log ("Silence Detected");
-						stopAudioClip ();
+						if (!clipRecorded) {
+							stopAudioClip ();
+							Debug.Log ("Next Question");
+							nextQuestion();
+						}
 					}
-
-					Debug.Log ("Next Question");
-
-					nextQuestion();
-
 				}
 			}
 		}
@@ -146,8 +151,9 @@ public class Questions : MonoBehaviour {
 	public void recordAudioClip(){
 		if (!userAnswered) {
 			//Maximum clip length of 3 minutes
-			currentClip = Microphone.Start (Microphone.devices [0], true, 180, 44100);
+			//currentClip = Microphone.Start (Microphone.devices [0], true, 180, 44100);
 			userAnswered = true;
+			Debug.Log ("userAnswered = true");
 		} 
 	}
 
@@ -157,11 +163,10 @@ public class Questions : MonoBehaviour {
 	public void stopAudioClip(){
 		clipRecorded = true;
 		clipCounter++;
-
-		//Then call stopAudioclip
-		Microphone.End(Microphone.devices[0]);
+		//Microphone.End(Microphone.devices[0]);
 		//Save Audio to file
-		SavWav.Save ("QuestionAudioClip" + clipCounter, currentClip);
+		Debug.Log("clipRecorded = true");
+		//SavWav.Save ("QuestionAudioClip" + clipCounter, currentClip);
 	}
 
 
@@ -194,11 +199,14 @@ public class Questions : MonoBehaviour {
 			currIndex = genRand.Next(0, phase2.Count-1);
 			GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase2[currIndex];
 			phase2.RemoveAt(currIndex);
+			//TODO: remove this
+			InterviewerSpeak(2, 1);
 			index2++;
 		}else if(index3 == 0){
 			//Phase 3
 			GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase3[0];
 			phase3.RemoveAt(0);
+			InterviewerSpeak(2, 1);
 			index3++;
 		}else{
 			GameObject.Find("QuestionText").GetComponent<Text>().text = "End of Questions";
@@ -211,9 +219,9 @@ public class Questions : MonoBehaviour {
 	void InterviewerSpeak(int phase, int question){
 		//TODO: Test out pausing audio clip in response to the user speaking, track number of pauses.
 		//Explore alternative responses to the user talking while the interviewer is speaking.
-		response = Resources.Load<AudioClip>("QuestionAudio/P1Q1");
+		response = Resources.Load<AudioClip>("QuestionAudio/P" + phase+ "Q" + question);
 		//AudioSource.PlayClipAtPoint (response, new Vector3(16, 1, 11));
-		AudioSource.PlayClipAtPoint (response, new Vector3(16, 1, 11), 0.5f);
+		AudioSource.PlayClipAtPoint (response, new Vector3(16, 1, 11), 0.1f);
 	}
 
 	void OnApplicationQuit(){
