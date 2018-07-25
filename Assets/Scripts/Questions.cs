ae701da9293e02ffcd540a73c9bfa24c44192615
maiv <maiv@wwu.edu>
2018-07-25 14:29:56 -0700

    Fixed silence detection and question flow

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -5,7 +5,9 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,15 +20,18 @@ public class Questions : MonoBehaviour {
 	List<string> phase1 = new List<string>();
 	List<string> phase2 = new List<string>();
 	List<string> phase3 = new List<string>();
+	bool endQuestions = false;
 
 	//Audioclip for interviewer responses
-	public int voiceSampleSize;
+	int voiceSampleSize;
 	bool interviewerSpeak = false; //Might not need this
 	AudioClip response;
 
 	//User Mic Input
 	public float sensitivity;
+	public int silenceTime;
 	int interruptCounter = 0;
+	Stopwatch stopwatch = new Stopwatch ();
 	AudioClip microphoneInput;
 	bool userAnswered = false;
 	bool interrupted = false;
@@ -42,6 +47,7 @@ public class Questions : MonoBehaviour {
 		string line;
 		string path;
 		path = Application.dataPath;
+		silenceTime = 4;
 		
 		//Read in phase 1 questions
 		System.IO.StreamReader file = new System.IO.StreamReader(path + "/Resources/Questions/Phase1.txt");
@@ -72,6 +78,10 @@ public class Questions : MonoBehaviour {
 		/* Setting up event listening for user response */
 		//EventManager.StartListening("interrupt", EventTest);
 		//EventManager.TriggerEvent ("Answered");
+
+		//Set detection variables to true to start detection, they will be reset later
+		userAnswered = true;
+		clipRecorded = true;
 		Invoke ("nextQuestion", 4); 
 	}
 
@@ -103,41 +113,47 @@ public class Questions : MonoBehaviour {
 				//Check if interviewer is talking (find the temp audiosource gameobject)
 				if (GameObject.Find ("One shot audio") && !interrupted) {
 					//User interrupted interviewer
-					Debug.Log("interrupt");
+					print ("interrupt");
 					interrupted = true;
-				} else if (!GameObject.Find ("One shot audio") && !userAnswered && !clipRecorded) {
+				} else if (GameObject.Find ("One shot audio") == null && !userAnswered && !clipRecorded) {
 					//If user hasn't answered, audio clip hasn't been recorded yet
 					//This should only be used once per question to begin the recording
-					Debug.Log("Recording clip");
+					print ("Recording clip");
 					recordAudioClip();
 				} 
 
-				//Debug.Log ("Level: " + level);
+				//print ("Level: " + level);
 			} else {
 				//User is not speaking
-				if (userAnswered && !GameObject.Find ("One shot audio")) {
+				if (userAnswered && GameObject.Find ("One shot audio") == null) {
 					//Audio has been recorded and User has answered the question 
 					//Check for silence
-					waveData = new float[500];
-					micPosition = Microphone.GetPosition (null) - (500 + 1); // null means the first microphone
+					stopwatch.Start();
+
+					waveData = new float[1000];
+					micPosition = Microphone.GetPosition (null) - (1000 + 1); // null means the first microphone
 					microphoneInput.GetData (waveData, micPosition);
 					//Check if user has not said anything during the last couple seconds before moving on
 					levelMax = 0;
 					wavePeak = 0;
-					for (int i = 0; i < 500; i++) {
+					for (int i = 0; i < 1000; i++) {
 						wavePeak = waveData [i] * waveData [i];
 						if (levelMax < wavePeak) {
 							levelMax = wavePeak;
 						}
 					}
-					level = Mathf.Sqrt (Mathf.Sqrt (levelMax));
 
-					if (level < sensitivity) {
-						Debug.Log ("Silence Detected");
-						if (!clipRecorded) {
+					level = Mathf.Sqrt (Mathf.Sqrt (levelMax));
+					TimeSpan ts = stopwatch.Elapsed;
+					//print ("Total time of silence " + ts.TotalSeconds);
+
+					if (level > sensitivity) {
+						stopwatch.Reset ();
+					} else {
+						//If x seconds of silence has passed, continue
+						if (!clipRecorded && userAnswered && ts.TotalSeconds >= silenceTime) {
 							stopAudioClip ();
-							Debug.Log ("Next Question");
-							nextQuestion();
+							nextQuestion ();
 						}
 					}
 				}
@@ -153,7 +169,7 @@ public class Questions : MonoBehaviour {
 			//Maximum clip length of 3 minutes
 			//currentClip = Microphone.Start (Microphone.devices [0], true, 180, 44100);
 			userAnswered = true;
-			Debug.Log ("userAnswered = true");
+			print ("userAnswered = true");
 		} 
 	}
 
@@ -165,7 +181,7 @@ public class Questions : MonoBehaviour {
 		clipCounter++;
 		//Microphone.End(Microphone.devices[0]);
 		//Save Audio to file
-		Debug.Log("clipRecorded = true");
+		print ("clipRecorded = true");
 		//SavWav.Save ("QuestionAudioClip" + clipCounter, currentClip);
 	}
 
@@ -175,41 +191,46 @@ public class Questions : MonoBehaviour {
 	 */
 	public void nextQuestion(){
 		int currIndex = 0;
-		//Reset detection flags
-		clipRecorded = false;
-		userAnswered = false;
 
 		if (interrupted) {
 			interruptCounter++;
-			Debug.Log ("User Interrupted: " + interruptCounter);
+			print ("User Interrupted: " + interruptCounter);
 			interrupted = false;
 		}
-
-		System.Random genRand = new System.Random();
-		//phase 1 questions
-		if (index1 == 0){
-			GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase1[0];
-			InterviewerSpeak(1, 1);
-			phase1.RemoveAt(0);
-			index1++;
-		}else if(index2 == 0 && index3 == 0){
-			//TODO: remove $$ index2 == 0 when we want more questions from random pool
-			//Check that we aren't in phase 3 
-			//Select random index from phase2
-			currIndex = genRand.Next(0, phase2.Count-1);
-			GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase2[currIndex];
-			phase2.RemoveAt(currIndex);
-			//TODO: remove this
-			InterviewerSpeak(2, 1);
-			index2++;
-		}else if(index3 == 0){
-			//Phase 3
-			GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase3[0];
-			phase3.RemoveAt(0);
-			InterviewerSpeak(2, 1);
-			index3++;
-		}else{
-			GameObject.Find("QuestionText").GetComponent<Text>().text = "End of Questions";
+			
+		if (!endQuestions && userAnswered && clipRecorded) {
+			print ("Next Question");
+			stopwatch.Reset ();
+			//Reset detection flags
+			clipRecorded = false;
+			userAnswered = false;
+			System.Random genRand = new System.Random();
+			//phase 1 questions
+			if (index1 == 0){
+				GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase1[0];
+				InterviewerSpeak(1, 1);
+				phase1.RemoveAt(0);
+				index1++;
+			}else if(index2 == 0 && index3 == 0){
+				//TODO: remove $$ index2 == 0 when we want more questions from random pool
+				//Check that we aren't in phase 3 
+				//Select random index from phase2
+				currIndex = genRand.Next(0, phase2.Count-1);
+				GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase2[currIndex];
+				phase2.RemoveAt(currIndex);
+				//TODO: remove this
+				InterviewerSpeak(2, 1);
+				index2++;
+			}else if(index3 == 0){
+				//Phase 3
+				GameObject.Find("QuestionText").GetComponent<Text>().text = " Current Question: " + phase3[0];
+				phase3.RemoveAt(0);
+				InterviewerSpeak(2, 1);
+				index3++;
+			}else{
+				GameObject.Find("QuestionText").GetComponent<Text>().text = "End of Questions";
+				endQuestions = true;
+			}
 		}
 	}
 
@@ -227,8 +248,10 @@ public class Questions : MonoBehaviour {
 	void OnApplicationQuit(){
 		if (Microphone.devices.Length != 0) {
 			//Save Audio to file
-			SavWav.Save ("microphoneInputTest", microphoneInput);
+
+			//TODO: uncomment this in final build
+			//SavWav.Save ("microphoneInputTest", microphoneInput);
 		}
-		Debug.Log("Total Questions Interrupted: " + interruptCounter);
+		print ("Total Questions Interrupted: " + interruptCounter);
 	}
 }
