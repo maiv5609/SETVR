9c51344887be248a3b5ed8daba569db621a1343b
maiv <maiv@wwu.edu>
2018-08-29 14:10:06 -0700

    Added Audio skipping to each question

---
+++ Assets/Scripts/AudioSkip.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class AudioSkip : MonoBehaviour {
+	private float[] timestamps = new float[10];
+	private String[] values;
+    public List<float> list = new List<float>();
+    // Use this for initialization
+    void Start () {
+        using (var reader = new StreamReader(Application.dataPath + "/Resources/Visualizations/responseTimestamps.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                
+                if(values[1] != "Timestamp (Minutes)")
+                {
+                    print(values[1]);
+                    list.Add(float.Parse(values[1]));
+                }
+            }
+        }
+    }
+
+    public void seek(int question)
+    {
+        print("Question: " + question + " , " + list[0]);
+
+        GameObject audio = GameObject.Find("MusicManager");
+        audio.GetComponent<HajiyevMusicManager>().seek(list[0]);
+    }
+
+}
